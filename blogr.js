dc114a8d0f51190a9e0aac149bfc70cc99ab8049
Mohammd1321 <69431624+Mohammd1321@users.noreply.github.com>
2021-05-15 07:19:59 +0300

    bloger-files

---
+++ blogr.js
@@ -0,0 +1,46 @@
+'use strict';
+const links = document.querySelectorAll('.target');
+
+const burger = document.querySelector('.burger');
+const closes = document.querySelector('.closes');
+const nav = document.querySelector('nav');
+
+
+links.forEach(link => {
+  link.addEventListener('click',function(e) {
+    e.preventDefault();
+    links.forEach(one => {
+      if(one !== link) {
+        addClass(one,true);
+      }
+    })
+    addClass(link)
+  })
+})
+
+function addClass(item,value = false) {
+  if(value) {
+    item.classList.remove('active');
+    item.nextElementSibling.classList.remove('active');
+  }else {
+    item.classList.toggle('active');
+    item.nextElementSibling.classList.toggle('active');
+  }
+}
+
+
+burger.addEventListener('click',function() {
+  display('none','block');
+  nav.style.top = '50%';
+})
+closes.addEventListener('click',function() {
+ display('block','none');
+ nav.style.top = '-20%';
+})
+
+
+function display(first,second) {
+  burger.style.display = first;
+  closes.style.display = second;
+}
+
